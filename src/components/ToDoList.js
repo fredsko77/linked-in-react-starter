a4f4c1d4ece0ea48630e6e95c26112a0711b1c3e
fagat <agathefrederick@gmail.com>
2020-02-19 07:36:03 +0100

    Update app code

--- src/components/ToDoList.js
+++ src/components/ToDoList.js
@@ -1,27 +1,38 @@
-import React from 'react'
+import React from 'react';
+import ToDo from './ToDo';
 
-const ToDoList = () => (
-    <React.Fragment>        
-        <h1 className="m-3">Liste de tâches</h1>
-        <ul className="list-group">
-            <li className="list-group-item d-flex align-items-center">
-                Ranger la vaisselle
-                <button className="btn btn-sm ml-auto btn-outline-success">&#x2713;</button>
-            </li>
-            <li className="list-group-item d-flex align-items-center">
-                Répondre appel d'offres
-                <button className="btn btn-sm ml-auto btn-outline-success">&#x2713;</button>
-            </li>
-            <li className="list-group-item d-flex align-items-center">
-                Signer contrat
-                <button className="btn btn-sm ml-auto btn-outline-success">&#x2713;</button>
-            </li>
-            <li className="list-group-item d-flex align-items-center">
-                Ranger le salon
-                <button className="btn btn-sm ml-auto btn-outline-success">&#x2713;</button>
-            </li>
-        </ul>
-    </React.Fragment>
-)
+const ToDoList = ({tasks, match, onToggleCompleted}) => {
+    let filteredTasks
+
+    switch (match.params.filter){
+        case 'completed':
+            filteredTasks = tasks.filter(task => task.completed)
+        break;
+        default: 
+            filteredTasks = tasks
+    }
+
+    if(filteredTasks.length === 0){
+        return (
+            <>
+                <h1 className="m-3">Liste de tâches</h1>
+                <ul className="list-group m-3">
+                    <li className="list-group-item">Aucune tâche à afficher</li>
+                </ul>
+            </>
+        )
+    } else {
+        return (
+            <>
+                <h1 className="m-3">Liste de tâches</h1>
+                <ul className="list-group m-3">
+                    {
+                        filteredTasks.map((task) => <ToDo task={task} key={task.id} onToggleCompleted={onToggleCompleted} />)
+                    }
+                </ul>
+            </>
+        )
+    }  
+}
 
 export default ToDoList
